03b2e080cf81c304193968ad99aa6a3a5f52d3a4
Martin Reese <bitbucket@reeses.eu>
2019-01-24 19:25:50 +0000

    V0.94 added "Verwaltungskosten d. Vertrages" and "Verkauf wegen Depotentgelt"

--- quickenImporter.rb
+++ quickenImporter.rb
@@ -13,23 +13,28 @@
 #
 # Version History
 # 23.05.2015: V0.01 initial version, basic ruby setup, reading file
-# 15.07.2015: V0.02 basic output creation finished 
+# 15.07.2015: V0.02 basic output creation finished
 # 20.07.2015: V0.03 DWS transaction csv input format implemented
-# 21.07.2015: V0.04 QIF format implemented; not yet checked for completeness 
+# 21.07.2015: V0.04 QIF format implemented; not yet checked for completeness
 # 23.07.2015: V0.05 writing into output file, format conversions implemented; datefrom and dateto implemented
 # 27.07.2015: V0.90 minor corrections, unit tested successfully
 # 11.09.2015: V0.91 fixed bug in type 'Umschichtung' -> can be Kauf or Verkauf based on +- sign
 # 24.01.2016: V0.92 forced encoding to iso-8859-1; introduced new transaction type 'Rueckforderung Zulage'
+# 10.02.2018: V0.93 added "Wiederanlage Ertragssteuer"
+# 24.01.2019: V0.94 added "Verwaltungskosten d. Vertrages" and "Verkauf wegen Depotentgelt"
 #
 #
 # Mapping infile to outfile transaction types:
 # "Beitrag" -> Kauf
 # "Umschichtung" -> Verkauf or Kauf, based on +- sign
-# "Depotentgeld" -> Verkauf
-# "Gutschrift Zulage" -> Kauf 
-# "Gutschrift Kinderzulage" -> Kauf 
+# "Depotentgelt" -> Verkauf
+# "Verwaltungskosten d. Vertrages" -> Verkauf
+# "Verkauf wegen Depotentgelt" -> Verkauf
+# "Gutschrift Zulage" -> Kauf
+# "Gutschrift Kinderzulage" -> Kauf
 # "Kauf VL zum Ausgabepreis" -> Kauf
 # "Wiederanlage der Ausschuettung" -> Retshrs
+# "Wiederanlage von Ertragsteuer" -> Retshrs
 # "Rueckforderung Zulage" -> Verkauf
 
 require 'date'
@@ -45,11 +50,11 @@ HELP = <<ENDHELP
    -v, --version    Show the version number.
    -i, --inspect    Inspect processing details.
    -f, --file       Specify the input file name.
-   -df,--datefrom	Convert transactions after or same as given date, ignore the rest 
-   -dt,--dateto		Convert transactions before or same as given date, ignore the rest 
+   -df,--datefrom	Convert transactions after or same as given date, ignore the rest
+   -dt,--dateto		Convert transactions before or same as given date, ignore the rest
 ENDHELP
 
-VERSION = "0.92"
+VERSION = "0.94"
 
 # FILE FORMATS
 IN_NO_OF_ATTRIBUTES = 9
@@ -88,21 +93,21 @@ ARGV.each do |arg|
 	  else
         puts USAGE
 		break
-      end		
+      end
   end
 end
 if filename == nil
   puts USAGE
   Kernel.exit
 end
-  
+
 # Read input file
 if filename
   file = File.new(filename, "r")
   lines = file.readlines
   if inspect
     puts "Datei eingelesen:\n"
-    lines.each do |line| 
+    lines.each do |line|
       puts line
 	end
   end
@@ -141,8 +146,8 @@ if inspect
 end
 # Map input to output records
 out_records = Array.new													# array of hashes
-in_records = in_records.drop(1)											# remove header line 
-in_record_counter = in_record_counter - 1 
+in_records = in_records.drop(1)											# remove header line
+in_record_counter = in_record_counter - 1
 in_records.each do |record|
 #  out_record = out_structure											# does not work
   date = Date.strptime(record[:preistag].to_s, "%d.%m.%Y")
@@ -153,35 +158,41 @@ in_records.each do |record|
     out_record[:V] = date													# valuta date
     case record[:umsatzart]												# transaction type
       when "Beitrag"
-	    out_record[:N] = "Kauf"
+	      out_record[:N] = "Kauf"
       when "Umschichtung"
-	    if record[:betrag].to_s.gsub(',', '.').to_f > 0 then 
-		  out_record[:N] = "Kauf"
-		else
-		  out_record[:N] = "Verkauf"
+        if record[:betrag].to_s.gsub(',', '.').to_f > 0
+		      out_record[:N] = "Kauf"
+		    else
+		      out_record[:N] = "Verkauf"
         end
       when "Depotentgelt"
-	    out_record[:N] = "Verkauf"
+	      out_record[:N] = "Verkauf"
+      when "Verwaltungskosten d. Vertrages"
+        out_record[:N] = "Verkauf"
+      when "Verkauf wegen Depotentgelt"
+        out_record[:N] = "Verkauf"
       when "Rueckforderung Zulage"
-	    out_record[:N] = "Verkauf"
-	  when "Gutschrift Zulage"
-	    out_record[:N] = "Kauf"
-	  when "Gutschrift Kinderzulage"
-	    out_record[:N] = "Kauf"
-	  when "Kauf VL zum Ausgabepreis"
-	    out_record[:N] = "Kauf"
-	  when "Wiederanlage der Ausschuettung"
-	    out_record[:N] = "Retshrs"
-	  else
-	    puts "Unknown Transaction: " + record[:umsatzart]
-	    exit
+	      out_record[:N] = "Verkauf"
+  	  when "Gutschrift Zulage"
+  	    out_record[:N] = "Kauf"
+  	  when "Gutschrift Kinderzulage"
+  	    out_record[:N] = "Kauf"
+  	  when "Kauf VL zum Ausgabepreis"
+  	    out_record[:N] = "Kauf"
+  	  when "Wiederanlage der Ausschuettung"
+  	    out_record[:N] = "Retshrs"
+  	  when "Wiederanlage von Ertragsteuer"
+  	    out_record[:N] = "Retshrs"
+  	  else
+  	    puts "Unknown Transaction: " + record[:umsatzart]
+  	    exit
     end
-	
-	record[:betrag]  = record[:betrag].to_s.gsub(',', '.').to_f.abs 		# always use positive values (transaction type indicates increase or decrease of shares) 
-    record[:anteile] = record[:anteile].to_s.gsub(',', '.').to_f.abs	
+
+	record[:betrag]  = record[:betrag].to_s.gsub(',', '.').to_f.abs 		# always use positive values (transaction type indicates increase or decrease of shares)
+    record[:anteile] = record[:anteile].to_s.gsub(',', '.').to_f.abs
     record[:preis]   = record[:preis].to_s.gsub(',', '.').to_f.abs
 
-	if (record[:umsatzart] == "Depotentgelt") then
+	if ((record[:umsatzart] == "Depotentgelt") || (record[:umsatzart] == "Verwaltungskosten d. Vertrages") || (record[:umsatzart] == "Verkauf wegen Depotentgelt"))
 	  out_record[:E] = "Depotkosten:Depotgebühren"
 	  out_record[:O] = "0.00|0.00|0.00|0.00|0.00|0.00|0.00|" + record[:betrag].to_s + "|0.00|0.00|0.00|0.00"
 	else
@@ -190,15 +201,15 @@ in_records.each do |record|
     out_record[:F] = record[:waehrung]									# currency
     out_record[:I] = record[:preis]										# price of a share
     out_record[:Q] = record[:anteile]									# number of shares
-    out_record[:L] = out_record[:N] == "Verkauf" ? "Kursgewinne:Realisierte Gewinne" : out_record[:N] == "Retshrs" ? "Kapitalerträge:sonstige Einnahme" : ""              
+    out_record[:L] = out_record[:N] == "Verkauf" ? "Kursgewinne:Realisierte Gewinne" : out_record[:N] == "Retshrs" ? "Kapitalerträge:sonstige Einnahme" : ""
     fondsname = record[:fondsname].split('/')							# fonds name
-    if (fondsname[1]) then
-      out_record[:Y] = fondsname[1].strip				
-	  out_record[:L] << "|[" + fondsname[0].strip + "]"					# category or transfer and (optionally) Class
-	else
-      out_record[:Y] = fondsname[0].strip				
-	end
-	
+    if (fondsname[1])
+      out_record[:Y] = fondsname[1].strip
+	    out_record[:L] << "|[" + fondsname[0].strip + "]"					# category or transfer and (optionally) Class
+	  else
+      out_record[:Y] = fondsname[0].strip
+    end
+
     out_records << out_record
   end
 end
@@ -218,15 +229,15 @@ printf(file, "!Type:Invst\n")
 out_records.each do |record|
   out_record_counter = out_record_counter + 1
   record.each do |key, value|
-    if inspect
+  if inspect
       print key
       print value
 	  print OUT_DELIMITER
-	end
+  end
 	printf(file, "%s%s%s", key, value, OUT_DELIMITER)
   end
   if inspect
-    puts "^"  
+    puts "^"
   end
   printf(file, "^\n")
 end
